9ec0c4a8fec18e2cbdaea134cf80ef41b05b3962
Md.Muntasir Raiyan <mdmuntasirraiyan@gmail.com>
2026-02-24 00:20:26 +0600

    Create largest_number.py

---
+++ python program/largest_number.py
@@ -0,0 +1,14 @@
+a=int(input("Enter your first number:"))
+b=int(input("Enter your second number:"))
+c=int(input("Enter your third number:"))
+if (a>b and a>c):
+    print("Fist number is the biggest number")
+elif (b>a and b>c):
+    print("Second number is the biggest number")
+else:
+    print("Third number is the biggest number")
+
+
+
+
+
