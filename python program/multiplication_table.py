32e4e5c5d9e9cf8259fe977c39cfb0a8172488f2
Md.Muntasir Raiyan <mdmuntasirraiyan@gmail.com>
2026-02-25 07:14:36 +0600

    Create multiplication_table.py

---
+++ python program/multiplication_table.py
@@ -0,0 +1,6 @@
+n=int(input("Enter number for multiplication table:"))
+print("The multiplication table is given below:")
+count=1 
+while count<=10:
+    print(n,"x",count,"=",n*count)
+    count=count+1
