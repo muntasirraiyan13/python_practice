62a97f143119a7c858079d2ed9a4605b495d2689
Md.Muntasir Raiyan <mdmuntasirraiyan@gmail.com>
2026-03-05 20:03:23 +0600

    Create class_object.py

---
+++ python program/class_object.py
@@ -0,0 +1,10 @@
+class Car:
+    brand="Mercedes"
+    model="G-Class"
+    colour="Blue"
+    made_in="Bnagladesh"
+car1=Car()
+print(car1.brand)
+print(car1.model)
+print(car1.colour)
+print(car1.made_in)
