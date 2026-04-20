365647d8e5ec93620ee7f58dd48ab84d2b11f618
Md.Muntasir Raiyan <mdmuntasirraiyan@gmail.com>
2026-04-21 00:04:07 +0600

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,8 @@
+multiplication= lambda x: x*2
+addition= lambda x,y: x+y
+divison= lambda x,y: x/y
+subtract= lambda x,y: x-y
+print(multiplication(2))
+print(addition(2,3))
+print(divison(4,2))
+print(subtract(10,5))
